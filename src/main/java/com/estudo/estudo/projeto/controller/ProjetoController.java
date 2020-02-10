c2174401310531eee5e7dc12438eb4042b94b23f
pedroventura88 <pedro.asventura@gmail.com>
2020-02-09 21:30:42 -0300

    Inserção do @GetMapping no metodo atualizar.

--- src/main/java/com/estudo/estudo/projeto/controller/ProjetoController.java
+++ src/main/java/com/estudo/estudo/projeto/controller/ProjetoController.java
@@ -44,6 +44,7 @@ public class ProjetoController {
         return "redirect:/projetos/listar";
     }
 
+    @GetMapping("atualizar")
     public String atualizar (@PathVariable("id") Integer id, Model model){
         model.addAttribute("projeto", pService.obterPorId(id));
         return "projeto/cadastrar";
