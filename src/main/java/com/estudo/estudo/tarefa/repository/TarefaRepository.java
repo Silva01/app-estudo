fa5c5c142b90b48480454c608e43bb3c36fafa24
pedroventura88 <pedro.asventura@gmail.com>
2020-02-11 20:59:53 -0300

    Criação da TarefaRepository.java

---
+++ src/main/java/com/estudo/estudo/tarefa/repository/TarefaRepository.java
@@ -0,0 +1,6 @@
+package com.estudo.estudo.tarefa.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TarefaRepository extends CrudRepository<TarefaRepository, Integer> {
+}
