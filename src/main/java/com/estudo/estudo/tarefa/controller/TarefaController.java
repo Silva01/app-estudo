dc42d4173574b1391a9d0a39f7119f4874a9b43b
pedroventura88 <pedro.asventura@gmail.com>
2020-02-11 21:51:59 -0300

    Inserção do RequestMapping no metodo salvar.

--- src/main/java/com/estudo/estudo/tarefa/controller/TarefaController.java
+++ src/main/java/com/estudo/estudo/tarefa/controller/TarefaController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping ("tarefas")
@@ -22,6 +23,7 @@ public class TarefaController {
         return "tarefa/cadastrar";
     }
 
+    @RequestMapping(path = "salvar", method = RequestMethod.POST)
     public String salvar (TarefaEntity tarefa){
         tService.salvar(tarefa);
         return "redirect:/tarefas/listar";
