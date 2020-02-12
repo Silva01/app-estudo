86ef2ec4ccae8c09e1b93faca893b5730efe2b6a
pedroventura88 <pedro.asventura@gmail.com>
2020-02-11 21:00:39 -0300

    Inserção da anotation @Repository

--- src/main/java/com/estudo/estudo/tarefa/repository/TarefaRepository.java
+++ src/main/java/com/estudo/estudo/tarefa/repository/TarefaRepository.java
@@ -1,6 +1,8 @@
 package com.estudo.estudo.tarefa.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TarefaRepository extends CrudRepository<TarefaRepository, Integer> {
 }
