23467b3cf3a60ef422978a63049006af0f2ec491
pedroventura88 <pedro.asventura@gmail.com>
2020-03-28 18:38:22 -0300

    Refatoração da Classe TarefaController.java

--- src/main/java/com/estudo/estudo/tarefa/controller/TarefaController.java
+++ src/main/java/com/estudo/estudo/tarefa/controller/TarefaController.java
@@ -1,6 +1,7 @@
 package com.estudo.estudo.tarefa.controller;
 
 import com.estudo.estudo.tarefa.entity.TarefaEntity;
+import com.estudo.estudo.tarefa.service.TarefaActionsEnun;
 import com.estudo.estudo.tarefa.service.TarefaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,34 +19,33 @@ public class TarefaController {
     private TarefaService tService;
 
     @GetMapping("cadastrar")
-    public String inserir (Model model) {
-        TarefaEntity tarefa = new TarefaEntity();
-        model.addAttribute("tarefa", tarefa);
-        return "tarefa/cadastrar";
+    public String createAndRedirectTask (Model model) {
+        return tService.getTaskById(0)
+                .uploadAttributesModel(model)
+                .redirect(TarefaActionsEnun.NEW_TASK_PAGE);
     }
 
     @RequestMapping(path = "salvar", method = RequestMethod.POST)
-    public String salvar (TarefaEntity tarefa){
-        tService.salvar(tarefa);
-        return "redirect:/tarefas/listar";
+    public String saveAndRedirect (TarefaEntity tarefa) {
+        return tService.salvar(tarefa).redirect(TarefaActionsEnun.REDIRECT_TASK);
     }
 
     @GetMapping("listar")
-    public String listar (Model model){
-        model.addAttribute("tarefasLista", tService.listarTodos());
-        return "tarefa/listar";
+    public String listAndRedirect (Model model) {
+        return tService.getAllTasks()
+                .uploadAttributesModel(model)
+                .redirect(TarefaActionsEnun.LIST_TASKS_PAGE);
     }
 
     @RequestMapping(path = "/deletar/{id}")
-    public String deletar (@PathVariable("id") Integer id, Model model) {
-        tService.deletar(id);
-        model.addAttribute("tarefa", tService.listarTodos());
-        return "redirect:/tarefas/listar";
+    public String deleteAndGetUserAndRedirect (@PathVariable("id") Integer id, Model model) {
+        return tService.deleteTaskById(id).getAllTasks().uploadAttributesModel(model)
+                .redirect(TarefaActionsEnun.REDIRECT_TASK);
     }
 
     @GetMapping("atualizar/{id}")
-    public String atualizar (@PathVariable("id") Integer id, Model model) {
-        model.addAttribute("tarefa", tService.obterPorId(id));
-        return "tarefa/cadastrar";
+    public String updateAndRedirect (@PathVariable("id") Integer id, Model model) {
+        return tService.getTaskById(id).uploadAttributesModel(model)
+                .redirect(TarefaActionsEnun.NEW_TASK_PAGE);
     }
 }
