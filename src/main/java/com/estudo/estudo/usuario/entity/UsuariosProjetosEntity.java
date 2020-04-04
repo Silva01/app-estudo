94d270d481c389cfd70c332115fbf0b7b150743a
silva01 <silva20102@gmail.com>
2020-04-04 09:51:04 -0300

    Adicionado entidade para relacionamento de muitos para muitos.

---
+++ src/main/java/com/estudo/estudo/usuario/entity/UsuariosProjetosEntity.java
@@ -0,0 +1,35 @@
+package com.estudo.estudo.usuario.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "UsuariosProjetos")
+public class UsuariosProjetosEntity {
+
+    @Id
+    @Column(name = "idUsuario", nullable = false)
+    private Integer idUsuario;
+
+    @Id
+    @Column(name = "idProjeto", nullable = false)
+    private Integer idProjeto;
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Integer getIdProjeto() {
+        return idProjeto;
+    }
+
+    public void setIdProjeto(Integer idProjeto) {
+        this.idProjeto = idProjeto;
+    }
+}
